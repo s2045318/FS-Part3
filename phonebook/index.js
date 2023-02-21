525fd8b4cdb673161f86b6ddd3e2c1a91e1a7fc9
s2045318 <s2045318@ed.ac.uk>
2023-02-21 14:35:18 +0200

    testing for 3.13

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,39 +1,50 @@
 // node_modules/.bin/nodemon index.js
-console.log("'Hello' World server started")
+console.log("Hello World server started")
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+
+//let persons = [
+ //   { 
+  //    "id": 1,
+  //    "name": "Arto Hellas", 
+  //    "number": "040-123456"
+  //  },
+  //  { 
+   //   "id": 2,
+   //   "name": "Ada Lovelace", 
+   //   "number": "39-44-5323523"
+   // },
+   // { 
+   //   "id": 3,
+   //   "name": "Dan Abramov", 
+   //   "number": "12-43-234345"
+  //  },
+  //  { 
+  //    "id": 4,
+  //    "name": "Mary Poppendieck", 
+   //   "number": "39-23-6423122"
+   // }
+//]
+
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(result => {
+        response.json(result)
+        result.forEach(note => {
+            console.log(note)
+        })
+      })
   })
   
+
 app.get('/api/persons/:id', (request, response) => {
     console.log(`GET persons/${request.params}`)
     const id = Number(request.params.id)
@@ -104,7 +115,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
