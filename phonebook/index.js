c52b5c39b7ac34d39c99b51c81da3d84afbe25ef
s2045318 <s2045318@ed.ac.uk>
2023-02-21 15:26:06 +0200

    fixed a typo in line 48 of index.js

--- phonebook/index.js
+++ phonebook/index.js
@@ -10,31 +10,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-
-
-//let persons = [
- //   { 
-  //    "id": 1,
-  //    "name": "Arto Hellas", 
-  //    "number": "040-123456"
-  //  },
-  //  { 
-   //   "id": 2,
-   //   "name": "Ada Lovelace", 
-   //   "number": "39-44-5323523"
-   // },
-   // { 
-   //   "id": 3,
-   //   "name": "Dan Abramov", 
-   //   "number": "12-43-234345"
-  //  },
-  //  { 
-  //    "id": 4,
-  //    "name": "Mary Poppendieck", 
-   //   "number": "39-23-6423122"
-   // }
-//]
-
 app.get('/api/persons', (request, response) => {
     console.log('request sent for all persons')
     Person.find({}).then(result => {
@@ -70,7 +45,7 @@ app.post('/api/persons', (request, response) => {
         number: body.number
     })
     person.save().then(savedPerson => {
-        response.json(savePerson)
+        response.json(savedPerson)
       })
 })
 
