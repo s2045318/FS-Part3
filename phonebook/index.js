3caf69ae8e12c72cbd85c26a25651931aa359ea7
s2045318 <s2045318@ed.ac.uk>
2023-02-21 14:57:14 +0200

    deleted some functionality but get persons works

--- phonebook/index.js
+++ phonebook/index.js
@@ -36,6 +36,7 @@ app.use(express.static('build'))
 //]
 
 app.get('/api/persons', (request, response) => {
+    console.log('request sent for all persons')
     Person.find({}).then(result => {
         response.json(result)
         result.forEach(note => {
@@ -45,74 +46,6 @@ app.get('/api/persons', (request, response) => {
   })
   
 
-app.get('/api/persons/:id', (request, response) => {
-    console.log(`GET persons/${request.params}`)
-    const id = Number(request.params.id)
-    console.log(id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-      response.json(person)
-    } else {
-        response.status(404).end()
-    }
-})
-
-
-app.get('api/persons/:id', (request,response) => {
-    const id = Number(request.params.id)
-    console.log(id)
-
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    console.log(`${id} succesfully deleted`)
-    response.status(204).end()
-})
-
-
-const generateId = () => {
-    return Math.floor(Math.random() * (2**15))
-}
-
-const duplicateCheck = (person) => {
-    for (const p of persons) {
-        if (p.name == person.name) {
-            console.log('duplicate name')
-            return true
-        }
-    }
-    return false
-}
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-    console.log(body)
-    if (!body.name) {
-        return response.status(400).json({ 
-            error: 'name missing' 
-        })
-    }
-    if (!body.number) {
-        return response.status(400).json({ 
-            error: 'number missing' 
-        })
-    }
-    if (duplicateCheck(body.name)) {
-        console.log(body.name)
-        return response.status(400).json({ 
-            error: `${body.name} is already in the phonebook` 
-        })
-    }
-
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId(),
-    }
-    persons = persons.concat(person)
-    response.json(persons)
-})
 
 
 const PORT = process.env.PORT 
