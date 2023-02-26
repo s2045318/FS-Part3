b74c663065a48a149b89445979bc4dbba6fed986
s2045318 <s2045318@ed.ac.uk>
2023-02-26 13:56:44 +0200

    3.22 finished

--- phonebook/models/person.js
+++ phonebook/models/person.js
@@ -1,38 +1,34 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
-
 console.log('connecting to', url)
-
 mongoose.connect(url)
   .then(result => {
-    console.log('connected to MongoDB')
+    console.log(`connected to MongoDB at ${result}`)
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
-
 const personSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minLength: 3
-    },
-    number: {
-      type: String,
-      validate: {
-        validator:function(v) {
-          return /^\d{2,3}-\d*$/.test(v);
-        }
+  name: {
+    type: String,
+    minLength: 3
+  },
+  number: {
+    type: String,
+    validate: {
+      validator:function(v) {
+        return /^\d{2,3}-\d*$/.test(v)
       }
     }
+  }
 })
-
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 module.exports = mongoose.model('Person', personSchema)
